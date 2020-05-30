8ff5df44df623211823de00850d9a7865cd6df4e
lyq <524123601@qq.com>
2020-05-30 21:38:49 +0800

    2020年5月30日21点38分

--- assignment_3/0529_2.py
+++ assignment_3/0529_2.py
@@ -1,3 +1,18 @@
 """
 编写函数，模拟选择法排序
 """
+
+
+def selectionSort(sourceArray):
+    for index in range(0, len(sourceArray)):
+        currentMin = index  # 当前遍历中最小值的游标，初始化
+        for currentIndex in range(index + 1, len(sourceArray)):
+            if sourceArray[currentIndex] < sourceArray[currentMin]:
+                currentMin = currentIndex
+        # 单次遍历结束，按下标交换值
+        sourceArray[index], sourceArray[currentMin] = sourceArray[currentMin], sourceArray[index]
+
+
+targetArray = [7, 6, 1, 1, 5, 1, 2]
+selectionSort(targetArray)
+print(targetArray)
--- assignment_3/0529_3.py
+++ assignment_3/0529_3.py
@@ -1,3 +1,29 @@
 """
 编写函数，使用递归算法实现冒泡排序算法
 """
+
+
+def cocktailSort(arr):
+    orderArr = [True]  # 基本数据类型没有引用
+    indexStart, indexEnd = 0, len(arr) - 1  # 未排序的部分
+    while indexEnd - indexStart > 0:
+        order = orderArr[0]
+        if order:  # 正序冒泡
+            rangeStart, rangeEnd, stepLength = indexStart, indexEnd, 1
+        else:  # 逆序冒泡
+            rangeStart, rangeEnd, stepLength = indexEnd, indexStart, -1
+        for index in range(rangeStart, rangeEnd, stepLength):
+            if order and arr[index] > arr[index + 1]:
+                arr[index], arr[index + 1] = arr[index + 1], arr[index]
+            elif not order and arr[index] < arr[index - 1]:
+                arr[index], arr[index - 1] = arr[index - 1], arr[index]
+        if order:
+            indexEnd -= 1
+        else:
+            indexStart += 1
+        orderArr[0] = not order
+
+
+targetArray = [7, 6, 1, 1, 5, 1, 2]
+cocktailSort(targetArray)
+print(targetArray)
