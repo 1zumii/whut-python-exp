af0a7a895911b9cbf98f4f62053a26f2cd69e0e2
lyq <524123601@qq.com>
2020-06-02 21:24:12 +0800

    2020年6月2日21点24分

--- assignment_3/0529_1.py
+++ assignment_3/0529_1.py
@@ -4,6 +4,29 @@
 下面的代码定义了一个函数用来测试随机给定的4个数是否符合24点游戏规则，
 如果符合就输出所有可能的表达式。
 """
+
+
+def judgePoint24(nums):
+    if not nums:
+        return False
+
+    def helper(nums):
+        if len(nums) == 1:
+            return abs(nums[0] - 24) < 1e-6
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i != j:
+                    newNums = [nums[k] for k in range(len(nums)) if i != k != j]
+                    if helper(newNums + [nums[i] + nums[j]]): return True
+                    if helper(newNums + [nums[i] - nums[j]]): return True
+                    if helper(newNums + [nums[i] * nums[j]]): return True
+                    if nums[j] != 0 and helper(newNums + [nums[i] / nums[j]]):
+                        return True
+        return False
+
+    return helper(nums)
+
+
 input4num = input('input: ')
 inputList = input4num.split(' ')
-
+print(judgePoint24(list(map(int, inputList))))
