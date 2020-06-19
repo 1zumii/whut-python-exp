ad26503a5f83041f74e23526ea4c36be650ee93a
lyq <524123601@qq.com>
2020-06-19 12:22:20 +0800

    2020年6月19日12点22分

---
+++ assignment_6/initailData.py
@@ -0,0 +1,36 @@
+JUNIOR = 'JUNIOR'
+SENIOR = 'SENIOR'
+
+A = 'A'
+B = 'B'
+C = 'C'
+
+managers = {
+	'Jason': JUNIOR, 'Alex': JUNIOR, 'Daniel': JUNIOR, 'Mike': JUNIOR,
+	'Jessica': SENIOR, 'Ted': SENIOR, 'Denny': JUNIOR, 'Fiona': JUNIOR,
+	'Eric': JUNIOR, 'Max': SENIOR, 'William': SENIOR, 'Zoe': JUNIOR,
+	'Peter': JUNIOR, 'Billy': JUNIOR, 'Tony': SENIOR, 'Roy': SENIOR,
+	'Coco': JUNIOR, 'Bella': JUNIOR, 'Anna': JUNIOR, 'Dominic': JUNIOR,
+	'Rena': SENIOR
+}
+teamBelong = [
+	C, A, B, B, A, C, B, C, A, B, A, A, B, C, A, B, C, A, B, C, A
+]
+quarterSales = [
+	[
+		4411, 3539, 3565, 3787, 3455, 3422, 3255, 4283, 4765, 4112, 2863,
+		4045, 3913, 4216, 2629, 2629, 3217, 2014, 1549, 3362, 4716
+	],
+	[
+		4591, 3544, 3654, 3444, 4567, 4533, 4538, 4236, 3156, 3185, 3285,
+		3581, 4536, 4384, 3856, 2453, 3254, 2312, 2310, 3145, 1550
+	],
+	[
+		2736, 3465, 3566, 3576, 3778, 3422, 3090, 2345, 2218, 2218, 3267,
+		3156, 3452, 3459, 1799, 3126, 3176, 2564, 2341, 3210, 2845
+	],
+	[
+		3756, 5466, 4566, 4556, 5344, 4555, 4786, 4256, 2333, 2008, 3567,
+		3345, 3421, 4123, 4356, 2876, 3129, 3012, 3300, 3111, 4012
+	]
+]
